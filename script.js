bc0bcad2d6a2d19422d11f374d3243bb730f7c55
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-06-14 23:05:22 +0000

    sghsrh

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 console.log("Running code")
 
 //Variables
-
 let shoppingCart = [  ]
+let check = new set();
+const Toby = "Toby"
+const Smilk = "Smilk"
 
-//functions
-
-
-//other code
+//main code
 const OUTPUT = document.getElementById("Basket")
 
-function addToBasket() {
-shoppingCart.push("Item")
-OUTPUT.innerHTML += ("added it to cart")
+function addTobyToBasket() {
+   shoppingCart.push(Toby)
+   check.add(Toby)
+   function checkDuplicates(shoppingCart) {
+  return check(shoppingCart).size !== array.length
+ if (checkDuplicates() === true) {
+  console.log("Duplicates found!")
+} 
+}
+   OUTPUT.innerHTML += ("Toby ")
+   console.log(shoppingCart)
+}
+
+
+
+function addSmilkToBasket() {
+shoppingCart.push(Smilk)
+OUTPUT.innerHTML += ("Smilk ")
 console.log(shoppingCart)
 }
