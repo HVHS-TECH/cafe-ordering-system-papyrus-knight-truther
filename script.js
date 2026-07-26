062967d082538b0afb75d83c76c96f3dcdf159f7
papyrus-knight-truther <24278sk@hvhs.school.nz>
2026-07-26 10:52:06 +0000

    Jul 26, 2026, 10:52 AM

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ function addToBasket(STARFAIT, quantityForm, star) {
     else{
       
 let quantity = quantityForm.value;
+for
 STARFAIT.quantity = quantityForm.value;
       OUTPUT.innerHTML += quantity += STARFAIT.name += " " ; //space so the cart items have a space between them
       console.log(shoppingCart);
